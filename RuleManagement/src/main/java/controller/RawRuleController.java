1cdbc919338fd8f2836d71db1a527a8b84b6bbc1
saeedshokoohi <shokoohi_saeed@yahoo.com>
2015-12-19 00:21:09 +0330

    setting up web flow adding jrebel config startin the first page : create raw rule page

--- RuleManagement/src/main/java/controller/RawRuleController.java
+++ RuleManagement/src/main/java/controller/RawRuleController.java
@@ -11,28 +11,18 @@ import javax.inject.Named;
  * Created by saeed on 15/12/2015.
  */
 @Named
-@Scope("view")
 public class RawRuleController {
 
-    RmRawRule entity;
-
-    public RmRawRule getEntity() {
-        return entity;
-    }
-
-    public void setEntity(RmRawRule entity) {
-        this.entity = entity;
-    }
 
     @Inject
     RawRuleRepository rawRuleRepository;
 
-    public void save()
+    public void save(RmRawRule entity)
     {
        // RmRawRule entity=new RmRawRule();
         entity.setResourceType("DRL");
       //  entity.setRuleName("firstRule");
-        entity.setVersionNumber("1.0");
+     //   entity.setVersionNumber("1.0");
         rawRuleRepository.findAll();
         rawRuleRepository.save(entity);
     }
